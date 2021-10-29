645f9edc1b6f2dcb72903e44b3ca760af099bb4c
Henrique <lhscaldas@gmail.com>
2021-10-29 10:26:07 -0300

    criação do iTPN

---
+++ missions/iTPN/server.py
@@ -0,0 +1,10 @@
+import socket
+HOST = ''              # Endereco IP do Servidor
+PORT = 5000            # Porta que o Servidor esta
+udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+orig = (HOST, PORT)
+udp.bind(orig)
+while True:
+    msg, client = udp.recvfrom(1024)
+    print(client, msg)
+udp.close()
