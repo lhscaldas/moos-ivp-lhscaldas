4e7664cf9ad968fad90ee61652c2edd9880c3fc2
Henrique <lhscaldas@gmail.com>
2021-11-05 16:10:51 -0300

    sensores de range corrigidos

--- missions/shoreside/postProcessing.py
+++ missions/shoreside/postProcessing.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from os import name
 import matplotlib.pyplot as plt
 
@@ -14,7 +15,7 @@ def LogReader(log_file,var):
               x.append(float(p[-2]))
     return t, x
 
-def RangeLogReader(log_file,var,vname):
+def RangeSensorLogReader(log_file,var,vname):
     t = []
     x = []
     with open(log_file, 'r') as f:
@@ -32,6 +33,24 @@ def RangeLogReader(log_file,var,vname):
                     x.append(float(dist[1]))
     return t, x
 
+def RealRangeLogReader(log_file,var,vname):
+    t = []
+    x = []
+    with open(log_file, 'r') as f:
+        text = f.readlines()
+        for line in text[4:]:
+            p = line.split(' ')
+            p[:] = [x for x in p if x]
+            if var in p:
+                reports = p[3:-1]
+                for report in reports:
+                    info=report.split(',')
+                    if 'vname='+vname in info:
+                        dist = info[1].split('=')
+                        t.append(float(p[0])) 
+                        x.append(float(dist[1]))
+    return t, x
+
 def hdg_log(file,var):
     t, x=LogReader(file,var)
     plt.plot(t[1:],x[1:],label=var)
@@ -46,64 +65,31 @@ def spd_log(file,var):
 
 def range_log(file,target):
     plt.figure()
-    t,x = RangeLogReader(file,"USR_RANGE_REPORT",target)
-    plt.plot(t,x,label="Radar")
-    t,x = RangeLogReader(file,"USL_RANGE_REPORT",target)
-    plt.plot(t,x,label="Lidar")
-    t,x = RangeLogReader(file,"USR_RANGE_REPORT_GT",target)
-    plt.plot(t,x,"--",label="Real")
+    t,x = RangeSensorLogReader(file,"USR_RANGE_REPORT",target)
+
+    plt.plot(t,x,"-",label=f"Radar")
+    t,x = RangeSensorLogReader(file,"USL_RANGE_REPORT",target)
+
+    plt.plot(t,x,"-",label=f"Lidar")
+    t,x = RealRangeLogReader(file,"CONTACTS_RECAP",target)
+
+    plt.plot(t,x,"--",label=f"Real")
     str = "Distância de " + target + " medida por alfa"
     plt.title(str)
     plt.xlabel("tempo [s]")
     plt.ylabel("distância [m]")
     plt.legend()
+    # plt.axis([0,max(t),min(x)*0.8,max(x)*1.2])
     plt.show()
 
 
 if __name__ == "__main__":
-    file='missions/shoreside/Log/Log.alog'
+    file='Log/Log.alog'
     target="bravo"
     range_log(file,target)
     target="charlie"
     range_log(file,target)
-    target="delta"
-    range_log(file,target)
-
-
-
-
-# plt.figure(1)
-# hdg_log("DESIRED_HEADING")
-# hdg_log("REAL_HEADING")
-# plt.legend()
-# plt.title("PID de rumo")
-# plt.axis([0, 1300, 0, 400])
-# plt.show()
-
-# plt.figure(2)
-# hdg_log("GYRO_HEADING")
-# hdg_log("IMU_HEADING")
-# plt.title("Sensores de rumo")
-# plt.legend()
-# plt.axis([0, 1300, 0, 400])
-# plt.show()
-
-# plt.figure(3)
-# spd_log("DESIRED_SPEED")
-# spd_log("REAL_SPEED")
-# plt.legend()
-# plt.title("PID de velocidade")
-# plt.axis([0, 1300, -1, 7])
-# plt.show()
-
-# plt.figure(4)
-# spd_log("DVL_SPEED")
-# spd_log("IMU_SPEED")
-# spd_log("GPS_SPEED")
-# plt.legend()
-# plt.title("Sensores de velocidade")
-# plt.axis([0, 1300, -1, 7])
-# plt.show()
-
+    # target="delta"
+    # range_log(file,target)
 
 
